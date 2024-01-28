c3ce8f8f04d791a156f084b1408ac0214ef76e6d
Patrick Guss <patrickg@krcginc.com>
2024-01-27 22:04:05 -0800

    Fix and more standardize

---
+++ ESGHackathon2024/GraphExt/VendorMaint_Extension.cs
@@ -0,0 +1,21 @@
+﻿using PX.Data;
+using PX.Objects.AP;
+using PX.Data.BQL.Fluent;
+
+namespace ESGHackathon2024
+{
+    // Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
+    public class VendorMaint_Extension : PXGraphExtension<VendorMaint>
+    {
+        public SelectFrom<MATBAccountItemUsage>
+            .Where<MATBAccountItemUsage.bAccountID.IsEqual<Vendor.bAccountID.FromCurrent>>
+            .View ESGData;
+
+        protected void _(Events.RowInserting<MATBAccountItemUsage> e)
+        {
+            if (e.Row == null) return;
+
+            ESGData.SetValueExt<MATBAccountItemUsage.bAccountID>(e.Row, Base.CurrentVendor.Current?.BAccountID);
+        }
+    }
+}
