b23bd87be7951c9457d668aa0ce64da74a842d59
Patrick Guss <patrickg@krcginc.com>
2024-01-27 23:31:39 -0800

    Final Updates

--- ESGHackathon2024/Attributes/ESGGenders.cs
+++ ESGHackathon2024/Attributes/ESGGenders.cs
@@ -1,15 +1,12 @@
-﻿using PX.Data;
+﻿using System;
+using PX.Common;
+using PX.Data;
+using PX.Data.BQL;
 using PX.Objects.CR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ESGHackathon2024.Attributes
 {
     #region GendersAttribute
-
     public class ESGGendersAttribute : PXStringListAttribute
     {
         public const string Male = "M";
@@ -17,29 +14,27 @@ namespace ESGHackathon2024.Attributes
         public const string NonBinary = "N";
         public const string Unspecified = "X";
         public const string Other = "O";
+
+        [PXLocalizable(MATMessages.MATError)]
         public class Labels
         {
             public const string Male = "Male";
-
             public const string Female = "Female";
             public const string NonBinary = "NonBinary";
             public const string Unspecified = "Unspecified";
             public const string Other = "Other";
         }
+
         private readonly Type _titleField;
 
-        public ESGGendersAttribute(Type titleField)
-            : this()
+        public ESGGendersAttribute(Type titleField) : this()
         {
-            if (titleField == null) throw new ArgumentNullException("titleField");
-            _titleField = titleField;
+            _titleField = titleField ?? throw new ArgumentNullException("titleField");
         }
 
-        public ESGGendersAttribute()
-            : base(new[] { Male, Female ,NonBinary,Unspecified,Other }, new[] { Labels.Male, Labels.Female,Labels.NonBinary,Labels.Unspecified,Labels.Other })
-        {
-
-        }
+        public ESGGendersAttribute() : base(new[] { Male, Female, NonBinary, Unspecified, Other }
+            , new[] { Labels.Male, Labels.Female, Labels.NonBinary, Labels.Unspecified, Labels.Other })
+        { }
 
         public override void CacheAttached(PXCache sender)
         {
@@ -57,10 +52,10 @@ namespace ESGHackathon2024.Attributes
             var gender = sender.GetValue(e.Row, _FieldName);
             if (gender == null)
             {
-                var title = sender.GetValue(e.Row, _titleField.Name) as string;
+                string title = (string)sender.GetValue(e.Row, _titleField.Name);
                 if (title != null)
                 {
-                    object newVal = null;
+                    string newVal = null;
                     switch (title)
                     {
                         case TitlesAttribute.Mr:
@@ -72,20 +67,21 @@ namespace ESGHackathon2024.Attributes
                             newVal = Female;
                             break;
                     }
-                    sender.SetValue(e.Row, _FieldName, newVal);
+
+                    if (newVal != null) sender.SetValue(e.Row, _FieldName, newVal);
                 }
             }
         }
 
         private void RowUpdatedHandler(PXCache sender, PXRowUpdatedEventArgs e)
         {
-            var gender = sender.GetValue(e.Row, _FieldName);
-            var oldGender = sender.GetValue(e.OldRow, _FieldName);
-            var title = sender.GetValue(e.Row, _titleField.Name) as string;
-            var oldlTitle = sender.GetValue(e.OldRow, _titleField.Name) as string;
+            string gender = (string)sender.GetValue(e.Row, _FieldName);
+            string oldGender = (string)sender.GetValue(e.OldRow, _FieldName);
+            string title = (string)sender.GetValue(e.Row, _titleField.Name);
+            string oldlTitle = (string)sender.GetValue(e.OldRow, _titleField.Name);
             if (gender == oldGender && title != null && title != oldlTitle)
             {
-                object newVal = null;
+                string newVal = null;
                 switch (title)
                 {
                     case TitlesAttribute.Mr:
@@ -97,21 +93,20 @@ namespace ESGHackathon2024.Attributes
                         newVal = Female;
                         break;
                 }
+
                 if (newVal != null) sender.SetValue(e.Row, _FieldName, newVal);
             }
         }
 
-        public class male : PX.Data.BQL.BqlString.Constant<male>
+        public class male : BqlString.Constant<male>
         {
             public male() : base(Male) { }
         }
 
-        public class female : PX.Data.BQL.BqlString.Constant<female>
+        public class female : BqlString.Constant<female>
         {
             public female() : base(Female) { }
         }
     }
-
     #endregion
-
 }
